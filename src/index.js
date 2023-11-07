c4912df8f144dea9e771317a7a6f2391a0471332
sylvainbarreau <sylvain.barreau@iut-tlse3.fr>
2023-11-08 00:05:07 +0100

    retour version initiale

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
-//ex 0:
-console.log("javascript");
-const myApp = require("simple-hello-world-example");
- 
-const msg = myApp.printMsg(); // print and return " Hello World! " 
-
-// ex1:
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>Bonjour tout le monde</h1>");
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`PORT Listen At ${PORT}`);
-});
-
-//ex2
-app.use('/dream',express.static('public'));
-
+//exercice 0:
+console.log("message javascript du serveur");
