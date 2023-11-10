f5235c6743ad306e541da4caae7ca49843ef2f9a
Louna Boulade <113367888+louna-bou@users.noreply.github.com>
2023-11-10 13:27:37 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 //exercice 0:
 console.log("message javascript du serveur");
+
+var myApp = require("simple-hello-world-example");
+var msg = myApp.printMsg();
+
+let express = require("express");
+let app = express();
+
+app.use("/dream", express.static("public"));
+app.get("/form", function (req, res) {
+  res.sendfile("public/tva.html");
+});
+
+//   le formulaire ne doit pas avoir enctype=« multipart/form-data »
+let bodyParser = require("body-parser");
+// utiliser le module middleware de parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// route post formulaire
+app.post("/tva", function (req, res) {
+  var tva = req.body.HT * (req.body.TVA / 100);
+  var cal = req.body.HT * (1 + req.body.TVA / 100);
+  res.send(
+    `Prix HT : ${req.body.HT} | Prix TVA : ${tva} | TTC : ${parseInt(cal)}`,
+  );
+});
+
+app.listen(8080);
